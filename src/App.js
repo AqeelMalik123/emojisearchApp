bc8226e25a821f3205074c59e56ae41fa4406129
AqeelMalik <maqeel191@gmail.com>
2022-01-12 03:53:32 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import EmojiApp from "./container/emoji"
+const App = () => {
+
+  return (
+    <div>
+      
+  <EmojiApp/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/container/emoji/index.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import EmojiData from "../../emojiapi/emoji.json"
+import "./css/style.css"
+const EmojiApp = () => {
+  const [search,setSearch] = useState("");
+  const [data,setData] = useState([]);
+
+  useEffect(()=>{
+      const newData = EmojiData.filter(emoji => emoji.title.toLowerCase().includes(search.toLowerCase()));
+      setData(newData);
+  },[search])
+  
+
+
+  return (
+    <div className="mt-20">
+      
+      <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Emoji Search</a>
+          <form className="d-flex">
+            <input style={{display:"flex",width:"400px",alignItems:"center"}} className="form-control mx-2" type="text" placeholder="Search" value={search} value={search} onChange={(e) => setSearch(e.target.value)} aria-label="Search" />
+          </form>
+        </div>
+      </nav>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+        
+
+          {data.map(emoji =>
+          <div className="card" key={emoji.title}>
+          <div className="card-body" onClick={() => {navigator.clipboard.writeText(emoji.symbol);alert("Emoji Copy")}}>
+            {emoji.symbol} {emoji.title}
+          </div>
+        </div>
+        )}
+          </div>
+          <div className="col">
+          {data.map(emoji =>
+          <div className="card" key={emoji.title}>
+          <div className="card-body" onClick={() => {navigator.clipboard.writeText(emoji.symbol);alert("Emoji Copy")}}>
+            {emoji.symbol} {emoji.title}
+          </div>
+        </div>
+        )}
+            
+          </div>
+          <div className="col">
+          {data.map(emoji =>
+          <div className="card" key={emoji.title}>
+          <div className="card-body" onClick={() => {navigator.clipboard.writeText(emoji.symbol);alert("Emoji Copy")}}>
+            {emoji.symbol} {emoji.title}
+          </div>
+        </div>
+        )}
+    
+          </div>
+        </div>
+      </div>
+        
+      
+    </div>
+  )
+}
+
+export default EmojiApp
